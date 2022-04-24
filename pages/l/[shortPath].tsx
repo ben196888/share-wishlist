4fd0e34538fe226980612e4911c1359989c4a1c1
Ben Liu <ben196888@gmail.com>
2022-04-24 12:27:52 +0700

    feat(l/[shortPath]): add shortPathValue in the rendering

--- pages/l/[shortPath].tsx
+++ pages/l/[shortPath].tsx
@@ -1,12 +1,19 @@
+import { ref } from 'firebase/database';
 import { useRouter } from 'next/router';
+import { useObjectVal } from 'react-firebase-hooks/database'
+import { database as db } from '../../firebase/clientApp';
+import type { ShareWishlist } from '../../types';
 
 export default function ShortPath() {
   const router = useRouter()
   const { shortPath } = router.query
-
+  const shortPathPath = `/shortPaths/${shortPath}`
+  const [shortPathVal, loading, error] = useObjectVal<ShareWishlist.Path>(ref(db, shortPathPath))
   return (
     <>
-      Page {shortPath} mock
+      {error && `Error: ${error}`}
+      {loading && 'Loading...'}
+      {shortPathVal && JSON.stringify(shortPathVal)}
     </>
   )
 }
