8872f2ff16b5ecf767ec9f10c7afc44fdd117888
Ben Liu <ben196888@gmail.com>
2022-04-26 00:37:12 +0700

    feat(Layout): add link on header

--- components/Layout/LayoutHeader.tsx
+++ components/Layout/LayoutHeader.tsx
@@ -1,13 +1,16 @@
-import { Center, Heading, forwardRef } from '@chakra-ui/react'
+import { Center, Heading, forwardRef, Link } from '@chakra-ui/react'
+import NextLink from 'next/link'
 
 export type LayoutHeaderProps = {}
 
 const LayoutHeader = forwardRef<LayoutHeaderProps, 'div'>((props, ref) => {
   return (
     <Center ref={ref}>
-      <Heading as='h1' size='4xl'>
-        Wishlist
-      </Heading>
+      <NextLink href='/' passHref>
+        <Link>
+          <Heading as='h1' size='4xl'>Wishlist</Heading>
+        </Link>
+      </NextLink>
     </Center>
   )
 })
